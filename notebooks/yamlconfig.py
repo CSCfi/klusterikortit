6dd1a2a607f6566e28b6bf6c67a06e75bee22bd9
Markus Koskela <markus.koskela@csc.fi>
2023-02-15 10:38:58 +0200

    Fix a number of issues

--- notebooks/yamlconfig.py
+++ notebooks/yamlconfig.py
@@ -4,12 +4,13 @@ import os
 
 CONFIGFILE = "config.yml"
 
-def read_config():
+def read_config(verbose=False):
     assert os.path.isfile(CONFIGFILE), "Config file missing"
     with open(CONFIGFILE) as cfg:
           config = yaml.load(cfg, Loader=SafeLoader)
-    print('Config file contents:')
-    for c in config: 
-      print(c, end=" ")
-    print()
+    if verbose:
+      print('Config file contents:')
+      for c in config:
+        print(c, end=" ")
+      print()
     return config
